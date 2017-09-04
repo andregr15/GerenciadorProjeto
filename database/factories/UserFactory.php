60ea49a071e7cccd360d10c29b48e69bf77911ec
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2017-09-04 17:00:20 -0300

    fase 1 do projeto feita

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(CodeProject\User::class, function (Faker $faker) {
+    static $password;
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'password' => $password ?: $password = bcrypt('secret'),
+        'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(CodeProject\Entities\Client::class, function(Faker $faker){
+    return[
+        'nome'=>$faker->name,
+        'responsavel'=>$faker->firstName,
+        'email'=>$faker->unique()->freeEmail,
+        'telefone'=>$faker->phoneNumber,
+        'endereco'=>$faker->address,
+        'obs'=>$faker->sentence
+    ];
+});
